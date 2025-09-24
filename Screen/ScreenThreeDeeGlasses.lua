8ddc0ead0751d0396c4cb79eb7418cf310201982
nathan <nathanfalveyprv@gmail.com>
2025-09-24 22:11:38 +0100

    Fixed a missing variable

--- Screen/ScreenThreeDeeGlasses.lua
+++ Screen/ScreenThreeDeeGlasses.lua
@@ -15,6 +15,8 @@ function OnStop()
 end
 
 function OnTick()
+    playerPed = PLAYER_PED_ID()
+    if not DOES_ENTITY_EXIST(playerPed) or IS_PLAYER_SWITCH_IN_PROGRESS() or IS_PED_DEAD_OR_DYING(playerPed,0) then return end
     DRAW_RECT(1.0, 0.5, 1.0, 1.0, 0, 0, 255, 168)
     DRAW_RECT(0.0, 0.5, 1.0, 1.0, 255, 0, 0, 168)
     SET_FOLLOW_PED_CAM_VIEW_MODE(4)
