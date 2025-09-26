fddffdf16896602697ff36a153dd38598a71af79
nathan <nathanfalveyprv@gmail.com>
2025-09-26 13:21:15 +0100

    Make Ped Vision Worse

--- Peds/PedsMillenials.lua
+++ Peds/PedsMillenials.lua
@@ -18,6 +18,7 @@ function OnTick()
         if ped ~= PLAYER_PED_ID() and IS_PED_RUNNING_MOBILE_PHONE_TASK(ped) == false and IS_PED_IN_ANY_VEHICLE(ped, false) == false then
             TASK_USE_MOBILE_PHONE(ped, 1, 2)
             SET_PED_HIGHLY_PERCEPTIVE(ped, false)
+            SET_PED_SEEING_RANGE(ped, 0)
         end
     end
 end
