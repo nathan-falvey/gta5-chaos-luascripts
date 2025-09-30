b92c9dcbe6054a923d71b66faf2abfce4eca4803
Nathan Falvey <nathanfalveyprv@gmail.com>
2025-09-30 23:12:19 +0100

    Added Effect: Express Trains

---
+++ Vehs/VehicleExpressTrains.lua
@@ -0,0 +1,23 @@
+EffectInfo = { 
+    Name = "Express Trains",
+    EffectId = "vehs_expresstrains",
+    TimedType = "Normal",
+    EffectGroup = "_group_trafficspawner",
+    EffectCategory = "None"
+
+}
+
+function OnStart()
+end
+
+function OnStop()
+end
+
+function OnTick()
+    SET_RANDOM_TRAINS(true)
+    for _, vehicle in ipairs(GetAllVehicles()) do
+        if GET_VEHICLE_CLASS(vehicle) == 21 then
+            SET_TRAIN_SPEED(vehicle, 9999.0)
+        end
+    end
+end
